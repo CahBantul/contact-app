27d4458c875adc9219e2055f8fbf42816dbd983f
Fardan Nozami <fardannozami@gmail.com>
2021-12-22 03:25:08 +0700

    add, delete and detail contact done

--- app.js
+++ app.js
@@ -1,30 +1,77 @@
-const { describe } = require('yargs');
 const yargs = require('yargs');
-const { saveContact } = require('./contacts');
+const {
+  saveContact,
+  listContact,
+  detailContact,
+  deteleContact,
+} = require('./contacts');
 
+yargs
+  .command({
+    command: 'add',
+    describe: 'menambahkan contact baru',
+    builder: {
+      nama: {
+        describe: 'Nama lengkap',
+        demandOption: true,
+        type: 'string',
+      },
+      email: {
+        describe: 'email',
+        demandOption: false,
+        type: 'string',
+      },
+      noHP: {
+        describe: 'Nomor HP',
+        demandOption: true,
+        type: 'string',
+      },
+    },
+    handler(argv) {
+      const { nama, email, noHP } = argv;
+      saveContact(nama, email, noHP);
+    },
+  })
+  .demandCommand();
+
+// menampilkan daftar seluruh kontak
 yargs.command({
-  command: 'add',
-  describe: 'menambahkan contact baru',
+  command: 'list',
+  describe: 'menampilkan semua nama dan noHP',
+  handler() {
+    listContact();
+  },
+});
+
+// menampilkan detail sebuah kontak
+yargs.command({
+  command: 'detail',
+  describe: 'menampilkan detail sebuah kontak berdasarkan nama',
   builder: {
     nama: {
       describe: 'Nama lengkap',
       demandOption: true,
       type: 'string',
     },
-    email: {
-      describe: 'email',
-      demandOption: false,
-      type: 'string',
-    },
-    noHP: {
-      describe: 'Nomor HP',
+  },
+  handler(argv) {
+    detailContact(argv.nama);
+  },
+});
+
+// menghapus kontak berdasarkan nama
+yargs.command({
+  command: 'delete',
+  describe: 'menghapus sebuah kontak berdasarkan nama',
+  builder: {
+    nama: {
+      describe: 'Nama lengkap',
       demandOption: true,
       type: 'string',
     },
   },
   handler(argv) {
-    const { nama, email, noHP } = argv;
-    saveContact(nama, email, noHP);
+    deteleContact(argv.nama);
   },
 });
 
