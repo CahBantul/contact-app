b10f18c91d110551ef6191ccfe1e9e21b87576c3
Fardan Nozami <fardannozami@gmail.com>
2021-12-22 08:03:24 +0700

    add express

--- app.js
+++ app.js
@@ -1,78 +1,93 @@
-const yargs = require('yargs');
-const {
-  saveContact,
-  listContact,
-  detailContact,
-  deteleContact,
-} = require('./contacts');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-yargs
-  .command({
-    command: 'add',
-    describe: 'menambahkan contact baru',
-    builder: {
-      nama: {
-        describe: 'Nama lengkap',
-        demandOption: true,
-        type: 'string',
-      },
-      email: {
-        describe: 'email',
-        demandOption: false,
-        type: 'string',
-      },
-      noHP: {
-        describe: 'Nomor HP',
-        demandOption: true,
-        type: 'string',
-      },
-    },
-    handler(argv) {
-      const { nama, email, noHP } = argv;
-      saveContact(nama, email, noHP);
-    },
-  })
-  .demandCommand();
+// gunakan ejs
+app.set('view engine', 'ejs');
 
-// menampilkan daftar seluruh kontak
-yargs.command({
-  command: 'list',
-  describe: 'menampilkan semua nama dan noHP',
-  handler() {
-    listContact();
-  },
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-// menampilkan detail sebuah kontak
-yargs.command({
-  command: 'detail',
-  describe: 'menampilkan detail sebuah kontak berdasarkan nama',
-  builder: {
-    nama: {
-      describe: 'Nama lengkap',
-      demandOption: true,
-      type: 'string',
-    },
-  },
-  handler(argv) {
-    detailContact(argv.nama);
-  },
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
 
-// menghapus kontak berdasarkan nama
-yargs.command({
-  command: 'delete',
-  describe: 'menghapus sebuah kontak berdasarkan nama',
-  builder: {
-    nama: {
-      describe: 'Nama lengkap',
-      demandOption: true,
-      type: 'string',
-    },
-  },
-  handler(argv) {
-    deteleContact(argv.nama);
-  },
-});
+// const yargs = require('yargs');
+// const {
+//   saveContact,
+//   listContact,
+//   detailContact,
+//   deteleContact,
+// } = require('./contacts');
 
-yargs.parse();
+// yargs
+//   .command({
+//     command: 'add',
+//     describe: 'menambahkan contact baru',
+//     builder: {
+//       nama: {
+//         describe: 'Nama lengkap',
+//         demandOption: true,
+//         type: 'string',
+//       },
+//       email: {
+//         describe: 'email',
+//         demandOption: false,
+//         type: 'string',
+//       },
+//       noHP: {
+//         describe: 'Nomor HP',
+//         demandOption: true,
+//         type: 'string',
+//       },
+//     },
+//     handler(argv) {
+//       const { nama, email, noHP } = argv;
+//       saveContact(nama, email, noHP);
+//     },
+//   })
+//   .demandCommand();
+
+// // menampilkan daftar seluruh kontak
+// yargs.command({
+//   command: 'list',
+//   describe: 'menampilkan semua nama dan noHP',
+//   handler() {
+//     listContact();
+//   },
+// });
+
+// // menampilkan detail sebuah kontak
+// yargs.command({
+//   command: 'detail',
+//   describe: 'menampilkan detail sebuah kontak berdasarkan nama',
+//   builder: {
+//     nama: {
+//       describe: 'Nama lengkap',
+//       demandOption: true,
+//       type: 'string',
+//     },
+//   },
+//   handler(argv) {
+//     detailContact(argv.nama);
+//   },
+// });
+
+// // menghapus kontak berdasarkan nama
+// yargs.command({
+//   command: 'delete',
+//   describe: 'menghapus sebuah kontak berdasarkan nama',
+//   builder: {
+//     nama: {
+//       describe: 'Nama lengkap',
+//       demandOption: true,
+//       type: 'string',
+//     },
+//   },
+//   handler(argv) {
+//     deteleContact(argv.nama);
+//   },
+// });
+
+// yargs.parse();
