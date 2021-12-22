25b69b239c775fac70928f4c37cb484666b2ee71
Cah Bantul <fardan.nozami@gmail.com>
2021-12-22 15:21:31 +0700

    bagian 1 start

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 3000;
 
 // gunakan ejs
 app.set('view engine', 'ejs');
+app.use(expressLayouts); //third party middleware
+app.use(express.static('public')); // build in middleware
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    layout: 'layouts/main-layouts',
+    title: 'Home Page',
+  });
 });
 
 app.listen(port, () => {
